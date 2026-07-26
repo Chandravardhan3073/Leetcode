8ac1215393fdde49786e926a7f11ac472e06833d
chandraVardhan <vardhan3073@gmail.com>
2026-07-27 04:12:07 +0530

    Time: 3 ms (0%), Space: 8.5 MB (25%) - LeetHub

--- 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.cpp
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
-    string s;
-    int dp[20][200];
-    string ans;
-    bool dfs(string& s, int pos, int sum, int tight, int S, string& currStr) {
-        if (pos == s.size()) {
-            if (sum == S) {
-                ans = currStr;
-                return true;
-            } else {
-                return false;
-            }
+    int digitSum(int num){
+        int sum = 0;
+        while(num >0){
+            sum += num%10;
+            num /= 10;
         }
-        if (!tight && dp[pos][sum] != -1) {
-            return dp[pos][sum];
-        }
-        int limit = tight ? s[pos] - '0' : 9;
-        long long ans = 0;
-        for (int digit = limit; digit >= 0; digit--) {
-            if (digit + sum > S)
-                continue;
-            currStr.push_back(digit + '0');
-            if (dfs(s, pos + 1, sum + digit, tight && (limit == digit), S,currStr)) {
-                return true;
-            }
-            currStr.pop_back();
-        }
-        if (!tight)
-            dp[pos][sum] = 0;
-
-        return false;
+        return sum;
     }
-    int largestInteger(int n, int S) {
+    int largestInteger(int n, int s) {
+        if(s == 0)return 0;
+        if(n*9 < s)return -1;
+        int Num = pow(10,n) -1;
+        while (true) {
+            int sum = digitSum(Num);
+            if (sum == s)
+                return Num;
+            int diff = sum - s;
+            Num -= diff;
+        }
 
-        int num = pow(10, n) - 1;
-        if (9 * n < S)
-            return -1;
-        s = to_string(num);
-        memset(dp, -1, sizeof(dp));
-        dfs(s, 0, 0, true, S, ans);
-        return stoi(ans);
+        return -1;
     }
 };
