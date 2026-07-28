f54d1a4caac806a7b042ddd22ceebd881d3c28a8
chandraVardhan <vardhan3073@gmail.com>
2026-07-28 23:20:53 +0530

    Time: 30 ms (69.23%), Space: 75.7 MB (33.63%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -4,16 +4,20 @@ public:
         vector<int> freq(26,0);
         int n = s.size();
         string t;
-        string m;
+        
         for(int i =0;i<n;i++){
             freq[s[i]- 'a']++;
         }
+        string mid ;
         for(int i = 0;i<26;i++){
             t.append(freq[i]/2, char(i + 'a'));
+            if(freq[i] % 2 == 1){
+                mid = char(i+'a');
+            }
         }
-        for(int i=t.size()-1;i>=0;i--){
-            m.push_back(t[i]);
-        }
-        return (n % 2 == 0) ? t+m: t+s[n/2]+m;
+        string m = t;
+        reverse(m.begin(),m.end());
+        return  t+ mid + m;
     }
 };
+//middle character is the one with an odd frequency,
