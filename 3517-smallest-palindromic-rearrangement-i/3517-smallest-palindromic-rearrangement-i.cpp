103e640c38191251a24dc7056065810e00db0214
chandraVardhan <vardhan3073@gmail.com>
2026-07-28 23:33:34 +0530

    Time: 110 ms (24.39%), Space: 74.6 MB (41.54%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        vector<int> freq(26,0);
-        int n = s.size();
-        string t;
-        
-        for(int i =0;i<n;i++){
-            freq[s[i]- 'a']++;
-        }
-        string mid ;
-        for(int i = 0;i<26;i++){
-            t.append(freq[i]/2, char(i + 'a'));
-            if(freq[i] % 2 == 1){
-                mid = char(i+'a');
+            string t ;
+            for(int i =0;i<s.size()/2;i++){
+                t.push_back(s[i]);
             }
-        }
-        string m = t;
-        reverse(m.begin(),m.end());
-        return  t+ mid + m;
+            sort(t.begin(),t.end());
+            
+            string m = t;
+            reverse(m.begin(),m.end());
+            return (s.size() % 2 == 0) ? t+m : t+s[s.size()/2] +m ;
     }
 };
 //middle character is the one with an odd frequency,
+//take the left part ? sort it reverse it add it  
