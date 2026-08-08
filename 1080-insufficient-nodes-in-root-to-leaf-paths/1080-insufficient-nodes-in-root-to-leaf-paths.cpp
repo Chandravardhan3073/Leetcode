8c7e85d926f39e2c86f22ac3e1fe3cfabb999ef5
chandraVardhan <vardhan3073@gmail.com>
2026-08-09 00:03:10 +0530

    Time: 0 ms (100%), Space: 34.6 MB (38.69%) - LeetHub

---
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* dfs(TreeNode* root, int limit,int Sum){
+        if(root == NULL) return NULL;
+        Sum += root->val;
+        if(root->left == NULL && root->right == NULL){
+            if(Sum < limit){
+                return NULL;
+            }else{
+                return root;
+            }
+        }
+        TreeNode* lf = dfs(root->left,limit,Sum);
+        TreeNode* rf = dfs(root->right,limit,Sum);
+        
+        root->left = lf;
+        root->right = rf;
+        if(lf == NULL && rf == NULL){
+            return NULL;
+        }
+        return  root;
+    }
+    TreeNode* sufficientSubset(TreeNode* root, int limit) {
+        return dfs(root,limit,0);
+    }
+};
