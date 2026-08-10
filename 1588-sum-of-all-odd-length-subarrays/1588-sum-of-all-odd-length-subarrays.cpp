547395270c1aed883c29698941d43118d42190ff
chandraVardhan <vardhan3073@gmail.com>
2026-08-10 20:16:34 +0530

    Time: 0 ms (100%), Space: 11.1 MB (83.67%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-        int odd_sum = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            int sum = 0;
-            for (int j = i; j < arr.size(); j++) {
-                sum += arr[j];
-                if ((j - i + 1) % 2 == 1) {
-                    odd_sum += sum;
-                }
+        int n = arr.size();
+        int sum = 0;
+        for(int i=0 ;i<n ;i++){
+            int total = (n-i) * (i +1);
+            if(total % 2 == 0){
+                sum += (total/2) * arr[i];
+            }else{
+                sum += (total/2 +1) * arr[i];
             }
         }
-        return odd_sum;
+        return sum;
     }
-}; // brute force
+}; // calculate how many times the idx appears at start and end n-idx * idx+1         then for odd divide by 2 and add 1 for even divide by 2 and Multiply the number  with the value at idx and use a sum variabale to track the sum
