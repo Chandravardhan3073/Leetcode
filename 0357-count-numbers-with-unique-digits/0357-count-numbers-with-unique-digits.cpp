b5f86452a0e7db3cbec3b4c3e1a6063f01991797
chandraVardhan <vardhan3073@gmail.com>
2026-08-15 22:33:12 +0530

    Time: 0 ms (100%), Space: 7.9 MB (46.37%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void ReturnVec(vector<int>& dp) {
+    vector<int> dp;
+    Solution() {
+        dp.resize(9);
         int n = dp.size();
+        dp[0] = 1;
         dp[1] = 10;
         int w = 9;
         int num = 9;
-        for (int i = 2; i <n; i++) {
+        for (int i = 2; i < n; i++) {
             w *= num;
             num--;
-            dp[i] = dp[i-1] + w;
-        }   
-    }
-    int countNumbersWithUniqueDigits(int n) {
-        vector<int> dp(9,0);
-        dp[0] = 1;
-        if(n==0){
-            return dp[0];
+            dp[i] = dp[i - 1] + w;
         }
-        ReturnVec(dp);
-
-        return dp[n];
+    }
+    int countNumbersWithUniqueDigits(int n) { 
+        return dp[n]; 
     }
 };
