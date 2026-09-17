2e287197c86ed2b747645cfc77e7499cd329cc78
chandraVardhan <vardhan3073@gmail.com>
2026-09-18 00:33:18 +0530

    Time: 0 ms (100%), Space: 10 MB (97.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,11 +4,12 @@ public:
         if(nums.size() == 1){
             return nums[0];
         }
-        int prev2 = nums[0];
+        int prev2 = 0;
+        int prev = nums[0];
         int curri ;
-
-        int prev = max(nums[0],nums[1]);
-        for(int i =2;i<nums.size();i++){
+        
+        
+        for(int i =1;i<nums.size();i++){
             curri = max(prev,prev2+nums[i]);
             prev2 = prev; 
             prev = curri;//for next i 
