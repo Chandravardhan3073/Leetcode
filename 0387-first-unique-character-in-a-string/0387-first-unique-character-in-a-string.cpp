34ceea525272615bb0f85ae4dacccd71372d59a0
chandraVardhan <vardhan3073@gmail.com>
2026-09-25 18:02:25 +0530

    Time: 0 ms (100%), Space: 14.6 MB (86.6%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int> mp;
-        for(char c :s){
-            mp[c]++;
+        vector<int> freq(26,0);
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
         }
         for(int i=0;i<s.size();i++){
-            if(mp[s[i]] ==1){
+            if(freq[s[i]-'a'] ==1){
                 return i;
             }
         }
